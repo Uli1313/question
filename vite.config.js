db23cb154f25c79e2990baa55d064fd3efb7cf67
Uli <ru21041555@gmail.com>
2023-11-20 23:21:21 +0800

    再次修改 vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import { ViteEjsPlugin } from "vite-plugin-ejs";
 import { fileURLToPath } from "node:url";
 import path from "node:path";
 import { glob } from "glob";
-// 新增
-// import copy from "rollup-plugin-copy";
+import copy from "rollup-plugin-copy";
 
 import liveReload from "vite-plugin-live-reload";
 
@@ -32,11 +31,10 @@ export default defineConfig({
     liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
     moveOutputPlugin(),
-    // 新增
-    // copy({
-    //   targets: [{ src: "assets/**/*.json", dest: "dist/assets" }],
-    //   hook: "writeBundle",
-    // }),
+    copy({
+      targets: [{ src: "assets/**/*.json", dest: "dist/assets" }],
+      hook: "writeBundle",
+    }),
   ],
   server: {
     // 啟動 server 時預設開啟的頁面
